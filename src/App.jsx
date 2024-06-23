7096f198786e348ceb691a17ad5067f4d36179cf
competitveblood <shivangimasterblaster@gmail.com>
2024-06-23 23:15:03 +0530

    git commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
+import JobCard from "./components/JobCard"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Header />
       <SearchBar />
+      <JobCard />
     </div>
   );
 }
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchBar() {
   return (
-    <div>
+    <div className="flex gap-4 mt-10 justify-center">
         <select className='w-64 py-3 pl-4 bg-zinc font-semibold rounded-md'>
             <option value="" disabled hidden selected>Job Role</option>
             <option value="iOS Developer">iOS Developer</option>
@@ -11,6 +11,26 @@ function SearchBar() {
             <option value="Android Developer">Android Developer</option>
             <option value="Developer Advocate">Developer Advocate</option>
         </select>
+        <select className='w-64 py-3 pl-4 bg-zinc font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Job Type</option>
+            <option value="Full time">Full time</option>
+            <option value="Part time">Part time</option>
+            <option value="Contract">Contract</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Location</option>
+            <option value="On-site">On-site</option>
+            <option value="Remote">Remote</option>            <option value="Frontend Developer">On-site</option>
+            <option value="Hybrid">Hybrid</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Experience</option>
+            <option value="Fresher">Fresher</option>
+            <option value="Junior Level">Junior Level</option>
+            <option value="Mid-Levelr">Mid-Level</option>
+            <option value="Senior-Level">Senior-Level</option>
+        </select>
+        <button className='w-64 bg-blue-500 text-white font-bold py-3 rounded-md'>Search</button>
       
     </div>
   )
